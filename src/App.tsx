5ccaed3838f41838c6aba98884485270075fc5ca
aurelticot <aurelticot.dev@gmail.com>
2023-05-19 10:45:49 +0200

    Integrate app contexts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+/* eslint-disable formatjs/no-literal-string-in-jsx */
 import React from "react";
+import { useVerida } from "features/verida";
 
 export const App: React.FunctionComponent = () => {
-  return <></>;
+  const { connect, disconnect, did, isConnected, profile } = useVerida();
+
+  return (
+    <div>
+      <p>did: {did || "<not connected>"}</p>
+      {isConnected ? (
+        <button onClick={() => void disconnect()}>Disconnect</button>
+      ) : (
+        <button onClick={() => void connect()}>Connect</button>
+      )}
+      {isConnected && profile !== undefined ? (
+        <p> {JSON.stringify(profile)}</p>
+      ) : null}
+    </div>
+  );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "styles/index.css";
 import { App } from "./App";
+import { AppContextProviders } from "contexts";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <AppContextProviders>
+      <App />
+    </AppContextProviders>
   </React.StrictMode>
 );
