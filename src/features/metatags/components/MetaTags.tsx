39ef7569357624248f33aac53dd7a9ae309c038f
aurelticot <aurelticot.dev@gmail.com>
2023-05-30 16:22:21 +1000

    set up meta tags

---
+++ src/features/metatags/components/MetaTags.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Helmet } from "react-helmet-async";
+
+import { DEFAULT_META_DESCRIPTION, DEFAULT_META_TITLE } from "~/constants";
+
+type MetaTagsProps = {
+  title?: string;
+  description?: string;
+  children?: React.ReactNode;
+};
+
+export const MetaTags: React.FunctionComponent<MetaTagsProps> = (props) => {
+  const { title, description, children } = props;
+
+  const metaTitle = title
+    ? `${title} - ${DEFAULT_META_TITLE}`
+    : DEFAULT_META_TITLE;
+
+  const metaDescription = description || DEFAULT_META_DESCRIPTION;
+
+  return (
+    <Helmet prioritizeSeoTags>
+      <title>{metaTitle}</title>
+      <meta name="title" content={metaTitle} />
+      <meta property="og:title" content={metaTitle} />
+      <meta property="twitter:title" content={metaTitle} />
+
+      <meta name="description" content={metaDescription} />
+      <meta property="og:description" content={metaDescription} />
+      <meta property="twitter:description" content={metaDescription} />
+      {children}
+    </Helmet>
+  );
+};
