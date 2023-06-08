4ef98b31109b326dc4cc1f55f4fb529e281e0982
aurelticot <aurelticot.dev@gmail.com>
2023-06-08 12:36:18 +1000

    Extend Verida base record schema in Terms schema

--- src/features/termsconditions/schemas/index.ts
+++ src/features/termsconditions/schemas/index.ts
@@ -1,1 +1,13 @@
-export * from "./terms";
+import { z } from "zod";
+
+import { VeridaBaseRecordSchema } from "~/features/verida";
+
+// To keep in sync with the corresponding schema.json
+// TODO: Find a wayt to generate this from the schema.json
+export const TermsConditionsSchema = z.object({
+  status: z.enum(["unknown", "accepted", "rejected"]).default("unknown"),
+});
+
+export const TermsConditionsRecordSchema = VeridaBaseRecordSchema.extend(
+  TermsConditionsSchema.shape
+).passthrough();
