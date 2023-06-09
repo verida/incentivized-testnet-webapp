84ec9feafdd8a5f200f4009432c4d8d64a1135b2
aurelticot <aurelticot.dev@gmail.com>
2023-06-09 11:23:13 +1000

    update meta tag information

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -2,8 +2,10 @@ export const VERIDA_CONTEXT_NAME = "Verida Missions";
 export const APP_TITLE = "Verida Missions";
 
 /** Default application title for the meta tags */
-export const DEFAULT_META_TITLE = APP_TITLE;
+export const DEFAULT_META_TITLE =
+  "Verida Missions: Discover the power of self-sovereign identity and decentralized data on the Verida Network";
 
 // TODO: Defined localised messages
 /** Default application description for the meta tags */
-export const DEFAULT_META_DESCRIPTION = "Verida Missions";
+export const DEFAULT_META_DESCRIPTION =
+  "Participate in Verida Missions and explore the Verida Network on the incentivized testnet.";
