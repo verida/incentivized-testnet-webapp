ea37ccc2379932887ad751cc06ee4c1898c4c45f
aurelticot <aurelticot.dev@gmail.com>
2023-06-20 11:44:46 +1000

    Temporarily disable terms of use
    closes Disable Terms & Conditions #47

--- src/features/termsconditions/contexts/TermsConditionsContext.tsx
+++ src/features/termsconditions/contexts/TermsConditionsContext.tsx
@@ -1,7 +1,7 @@
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import React, { useCallback, useMemo } from "react";
 
-import { AcceptTermsAmdConditionsModal } from "~/components/organisms";
-import { useTermsConditionsQueries } from "~/features/termsconditions/hooks";
+// import { AcceptTermsAmdConditionsModal } from "~/components/organisms";
+// import { useTermsConditionsQueries } from "~/features/termsconditions/hooks";
 import { TermsConditionsStatus } from "~/features/termsconditions/types";
 import { getLatestTermsConditions } from "~/features/termsconditions/utils";
 import { useVerida } from "~/features/verida";
@@ -25,62 +25,62 @@ type TermsConditionsProviderProps = {
 export const TermsConditionsProvider: React.FunctionComponent<
   TermsConditionsProviderProps
 > = (props) => {
-  const [openModal, setOpenModal] = useState(false);
+  // const [openModal, setOpenModal] = useState(false);
   const { isConnected } = useVerida();
 
-  const { isCheckingStatus, status, saveStatus, deleteStatus } =
-    useTermsConditionsQueries();
+  // const { isCheckingStatus, status, saveStatus, deleteStatus } =
+  //   useTermsConditionsQueries();
 
-  useEffect(() => {
-    if (!isConnected || isCheckingStatus || status === "accepted") {
-      setOpenModal(false);
-    } else {
-      setOpenModal(true);
-    }
-  }, [isCheckingStatus, isConnected, status]);
+  // useEffect(() => {
+  //   if (!isConnected || isCheckingStatus || status === "accepted") {
+  //     setOpenModal(false);
+  //   } else {
+  //     setOpenModal(true);
+  //   }
+  // }, [isCheckingStatus, isConnected, status]);
 
   const openAcceptModal = useCallback(() => {
-    if (!isConnected || isCheckingStatus) {
+    // if (!isConnected || isCheckingStatus) {
+    if (!isConnected) {
       // Do not open if user not connected
       return;
     }
-    setOpenModal(true);
-  }, [isConnected, isCheckingStatus]);
+    // setOpenModal(true);
+  }, [isConnected]);
 
-  const updateStatus = useCallback(
-    (status: TermsConditionsStatus) => {
-      saveStatus(status);
-    },
-    [saveStatus]
-  );
+  const updateStatus = useCallback((_status: TermsConditionsStatus) => {
+    // saveStatus(status);
+  }, []);
 
   const deleteTermsStatus = useCallback(() => {
-    deleteStatus();
-  }, [deleteStatus]);
-
-  const handleCloseModal = useCallback(() => {
-    setOpenModal(false);
+    // deleteStatus();
   }, []);
+
+  // const handleCloseModal = useCallback(() => {
+  //   setOpenModal(false);
+  // }, []);
 
   const contextValue: TermsConditionsContextType = useMemo(
     () => ({
-      isCheckingStatus,
-      status: status ?? "unknown",
+      // isCheckingStatus,
+      isCheckingStatus: false,
+      // status: status ?? "unknown",
+      status: "accepted",
       updateStatus,
       deleteTermsStatus,
       getLatestVersion: getLatestTermsConditions,
       openAcceptModal,
     }),
-    [isCheckingStatus, status, updateStatus, deleteTermsStatus, openAcceptModal]
+    [updateStatus, deleteTermsStatus, openAcceptModal]
   );
 
   return (
     <TermsConditionsContext.Provider value={contextValue}>
       {props.children}
-      <AcceptTermsAmdConditionsModal
+      {/* <AcceptTermsAmdConditionsModal
         open={openModal}
         onClose={handleCloseModal}
-      />
+      /> */}
     </TermsConditionsContext.Provider>
   );
 };
