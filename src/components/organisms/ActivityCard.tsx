0158dc2f7ae43f3d70378b36f954ea0091e75135
aurelticot <aurelticot.dev@gmail.com>
2023-06-28 15:30:18 +1000

    Add debounce to execute activity

--- src/components/organisms/ActivityCard.tsx
+++ src/components/organisms/ActivityCard.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback, useState } from "react";
 import { useIntl } from "react-intl";
+import { useDebouncedCallback } from "use-debounce";
 
 import { Button, Typography } from "~/components/atoms";
 import { ActivityStatus } from "~/components/molecules";
@@ -36,21 +37,27 @@ export const ActivityCard: React.FunctionComponent<ActivityCardProps> = (
     void connect();
   }, [connect]);
 
-  const handleExecuteActivity = useCallback(async () => {
-    setExecuting(true);
-    try {
-      // executeActivity handlers errors by itself but surrounding by try/catch just in case something has been forgotten
-      await executeActivity(activity.id);
-    } catch (error: unknown) {
-      Sentry.captureException(error, {
-        tags: {
-          activityId: activity.id,
-        },
-      });
-    } finally {
-      setExecuting(false);
+  const handleExecuteActivity = useDebouncedCallback(
+    async () => {
+      setExecuting(true);
+      try {
+        // executeActivity handlers errors by itself but surrounding by try/catch just in case something has been forgotten
+        await executeActivity(activity.id);
+      } catch (error: unknown) {
+        Sentry.captureException(error, {
+          tags: {
+            activityId: activity.id,
+          },
+        });
+      } finally {
+        setExecuting(false);
+      }
+    },
+    1000,
+    {
+      leading: true,
     }
-  }, [executeActivity, activity.id]);
+  );
 
   const resourcesSectionTitle = i18n.formatMessage({
     id: "ActivityCard.resourcesSectionTitle",
