50a12b7bd002a6b200be5c0a7bb35faa6a6a732c
aurelticot <aurelticot.dev@gmail.com>
2023-06-28 14:59:34 +1000

    Optimise and secure activity execution handler

--- src/features/activity/contexts/ActivityContext.tsx
+++ src/features/activity/contexts/ActivityContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useCallback, useMemo } from "react";
+import { createContext, useCallback, useMemo, useRef } from "react";
 import { toast } from "react-hot-toast";
 import { useIntl } from "react-intl";
 
@@ -45,6 +45,7 @@ export const ActivityProvider: React.FunctionComponent<
     saveActivity,
     deleteActivities,
   } = useActivityQueries();
+  const executingActivityRef = useRef(false);
 
   // Initialise the activities
   // useEffect(() => {
@@ -99,158 +100,228 @@ export const ActivityProvider: React.FunctionComponent<
 
   const executeActivity = useCallback(
     async (activityId: string) => {
-      Sentry.addBreadcrumb({
-        category: "activity",
-        level: "info",
-        message: "Executing activity",
-        data: {
-          activityId,
-        },
-      });
-
-      if (statusTermsConditions !== "accepted") {
-        // Should be avoided by the UI but just in case
-        const termsNotAcceptedNotificationMessage = i18n.formatMessage({
-          id: "ActivityProvider.termsNotAcceptedNotificationMessage",
-          defaultMessage: "Please accept the terms and conditions",
+      if (executingActivityRef.current) {
+        const activityAlreadyBeingExecutedMessage = i18n.formatMessage({
+          id: "ActivityProvider.activityAlreadyBeingExecutedMessage",
+          defaultMessage:
+            "An activity is already being executed, please wait for it to finish",
           description:
-            "Notification message when user tries to execute an activity but the terms and conditions are not accepted",
+            "Notification message when user try to execute an activity while there's already one being executed",
         });
-        toast.error(termsNotAcceptedNotificationMessage);
-        Sentry.captureException(
-          new Error("Trying to execute an activity without terms accepted"),
-          {
-            tags: {
-              activityId,
-            },
-          }
-        );
+        toast.success(activityAlreadyBeingExecutedMessage, {
+          icon: null,
+        });
         return;
       }
 
-      const activity = activities.find((a) => a.id === activityId);
-
-      if (!activity) {
-        // Unlikely but sill
-        const activityNotFoundNotificationMessage = i18n.formatMessage({
-          id: "ActivityProvider.activityNotFoundNotificationMessage",
-          defaultMessage: "Activity not found",
-          description:
-            "Notification message when user tries to execute an activity but the activity is not found",
-        });
-        toast.error(activityNotFoundNotificationMessage);
-        Sentry.captureException(
-          new Error(
-            "Trying to execute an activity with an id that cannot be found"
-          ),
-          {
-            tags: {
-              activityId,
-            },
-          }
-        );
-        return;
-      }
-
-      // Check existing status, if todo, continue, otherwise return or throw error
-      const userActivity = userActivities?.find(
-        (activity) => activity.id === activityId
-      );
-
-      if (userActivity && userActivity.status !== "todo") {
-        // Should be avoided by the UI but just in case
-        const activityAlreadyExecutedNotificationMessage = i18n.formatMessage({
-          id: "ActivityProvider.activityAlreadyExecutedNotificationMessage",
-          defaultMessage: "Activity already executed",
-          description:
-            "Notification message when user tries to execute an activity but the activity is already executed",
-        });
-        toast.success(activityAlreadyExecutedNotificationMessage);
-        Sentry.captureException(
-          new Error("Trying to execute an activity already executed"),
-          {
-            tags: {
-              activityId,
-            },
-          }
-        );
-        return;
-      }
-
-      // Execute the action
-      const result = await activity.onExecute(webUserInstanceRef);
-
-      // Handle the result
-      switch (result.status) {
-        case "completed": {
-          const activityExecutionCompletedNotificationMessage =
-            i18n.formatMessage({
-              id: "ActivityProvider.activityExecutionCompletedNotificationMessage",
-              defaultMessage: "Congrats, you have completed the activity",
-              description:
-                "Notification message when user completes an activity",
-            });
-          toast.success(
-            result.message
-              ? i18n.formatMessage(result.message)
-              : activityExecutionCompletedNotificationMessage
-          );
-
-          capturePlausibleEvent("Activity Completed", {
+      executingActivityRef.current = true;
+      try {
+        Sentry.addBreadcrumb({
+          category: "activity",
+          level: "info",
+          message: "Executing activity",
+          data: {
             activityId,
-          });
-          break;
-        }
-        case "todo": {
-          const activityExecutionTodoNotificationMessage = i18n.formatMessage({
-            id: "ActivityProvider.activityExecutionTodoNotificationMessage",
-            defaultMessage: "Hmm, the activity was not completed properly",
+          },
+        });
+
+        if (statusTermsConditions !== "accepted") {
+          // Should be avoided by the UI but just in case
+          const termsNotAcceptedNotificationMessage = i18n.formatMessage({
+            id: "ActivityProvider.termsNotAcceptedNotificationMessage",
+            defaultMessage: "Please accept the terms and conditions",
             description:
-              "Notification message when executing an activity reset back to its todo status",
+              "Notification message when user tries to execute an activity but the terms and conditions are not accepted",
           });
-          toast.error(
-            result.message
-              ? i18n.formatMessage(result.message)
-              : activityExecutionTodoNotificationMessage
-          );
-          break;
-        }
-        case "pending": {
-          const activityExecutionPendingNotificationMessage =
-            i18n.formatMessage({
-              id: "ActivityProvider.activityExecutionPendingNotificationMessage",
-              defaultMessage:
-                "The activity is pending, check the next step in the instructions",
-              description:
-                "Notification message when after the execution, an activity is in pending state",
-            });
-          toast.success(
-            result.message
-              ? i18n.formatMessage(result.message)
-              : activityExecutionPendingNotificationMessage,
-            {
-              icon: null,
-            }
-          );
-          break;
-        }
-        default: {
+          toast.error(termsNotAcceptedNotificationMessage);
           Sentry.captureException(
-            new Error("Non supported status returned by activity.onExecute"),
+            new Error("Trying to execute an activity without terms accepted"),
             {
               tags: {
                 activityId,
               },
             }
           );
+          return;
         }
-      }
 
-      // Save the result
-      saveActivity({
-        id: activityId,
-        status: result.status,
-      });
+        const activity = activities.find((a) => a.id === activityId);
+
+        if (!activity) {
+          // Unlikely but sill
+          const activityNotFoundNotificationMessage = i18n.formatMessage({
+            id: "ActivityProvider.activityNotFoundNotificationMessage",
+            defaultMessage: "Activity not found",
+            description:
+              "Notification message when user tries to execute an activity but the activity is not found",
+          });
+          toast.error(activityNotFoundNotificationMessage);
+          Sentry.captureException(
+            new Error(
+              "Trying to execute an activity with an id that cannot be found"
+            ),
+            {
+              tags: {
+                activityId,
+              },
+            }
+          );
+          return;
+        }
+
+        // Check existing status, if todo, continue, otherwise return or throw error
+        const userActivity = userActivities?.find(
+          (activity) => activity.id === activityId
+        );
+
+        if (userActivity && userActivity.status !== "todo") {
+          // Should be avoided by the UI but just in case
+          const activityAlreadyExecutedNotificationMessage = i18n.formatMessage(
+            {
+              id: "ActivityProvider.activityAlreadyExecutedNotificationMessage",
+              defaultMessage: "Activity already executed",
+              description:
+                "Notification message when user tries to execute an activity but the activity is already executed",
+            }
+          );
+          toast.success(activityAlreadyExecutedNotificationMessage);
+          Sentry.captureException(
+            new Error("Trying to execute an activity already executed"),
+            {
+              tags: {
+                activityId,
+              },
+            }
+          );
+          return;
+        }
+
+        // Execute the action
+        let executionResult;
+        try {
+          executionResult = await activity.onExecute(webUserInstanceRef);
+        } catch (error: unknown) {
+          const errorWhileExecutionActivityMessage = i18n.formatMessage({
+            id: "ActivityProvider.errorWhileExecutionActivityMessage",
+            defaultMessage:
+              "Hmm, There was an executing the activity, please wait a moment and try again",
+            description:
+              "Notification message when user tries to execute an activity but there is an error while executing it",
+          });
+          toast.error(errorWhileExecutionActivityMessage);
+          Sentry.captureException(
+            new Error(
+              "There was a unhandled error while executing an activity"
+            ),
+            {
+              tags: {
+                activityId,
+              },
+            }
+          );
+          return;
+        }
+
+        // Save the execution result
+        try {
+          await saveActivity({
+            id: activityId,
+            status: executionResult.status,
+          });
+        } catch (error: unknown) {
+          const errorWhileSavingExecutionResultMessage = i18n.formatMessage({
+            id: "ActivityProvider.errorWhileSavingExecutionResultMessage",
+            defaultMessage:
+              "Hmm, There was an issue while saving the activity result, please wait a moment and try again",
+            description:
+              "Notification message when user tries to execute an activity but there is an error while saving the result",
+          });
+          toast.error(errorWhileSavingExecutionResultMessage);
+          // No need to capture the error with Sentry as it has been captured by the mutation error handler ... normally
+          return;
+        }
+
+        // Capture the completed activity in Plausible
+        if (executionResult.status === "completed") {
+          capturePlausibleEvent("Activity Completed", {
+            activityId,
+          });
+        }
+
+        // Notify about the result
+        switch (executionResult.status) {
+          case "completed": {
+            const activityExecutionCompletedNotificationMessage =
+              i18n.formatMessage({
+                id: "ActivityProvider.activityExecutionCompletedNotificationMessage",
+                defaultMessage: "Congrats, you have completed the activity",
+                description:
+                  "Notification message when user completes an activity",
+              });
+            toast.success(
+              executionResult.message
+                ? i18n.formatMessage(executionResult.message)
+                : activityExecutionCompletedNotificationMessage
+            );
+            break;
+          }
+          case "todo": {
+            const activityExecutionTodoNotificationMessage = i18n.formatMessage(
+              {
+                id: "ActivityProvider.activityExecutionTodoNotificationMessage",
+                defaultMessage: "Hmm, the activity was not completed properly",
+                description:
+                  "Notification message when executing an activity reset back to its todo status",
+              }
+            );
+            toast.error(
+              executionResult.message
+                ? i18n.formatMessage(executionResult.message)
+                : activityExecutionTodoNotificationMessage
+            );
+            break;
+          }
+          case "pending": {
+            const activityExecutionPendingNotificationMessage =
+              i18n.formatMessage({
+                id: "ActivityProvider.activityExecutionPendingNotificationMessage",
+                defaultMessage:
+                  "The activity is pending, check the next step in the instructions",
+                description:
+                  "Notification message when after the execution, an activity is in pending state",
+              });
+            toast.success(
+              executionResult.message
+                ? i18n.formatMessage(executionResult.message)
+                : activityExecutionPendingNotificationMessage,
+              {
+                icon: null,
+              }
+            );
+            break;
+          }
+          default: {
+            const nonSupportedExecutionResultMessage = i18n.formatMessage({
+              id: "ActivityProvider.nonSupportedExecutionResultMessage",
+              defaultMessage:
+                "Hmm, Something went wrong, please wait a moment and try again",
+              description:
+                "Notification message when the execution activity resulted in an unknown status",
+            });
+            toast.error(nonSupportedExecutionResultMessage);
+            Sentry.captureException(
+              new Error("Non supported status returned by activity.onExecute"),
+              {
+                tags: {
+                  activityId,
+                },
+              }
+            );
+          }
+        }
+      } finally {
+        executingActivityRef.current = false;
+      }
     },
     [
       i18n,
@@ -268,9 +339,9 @@ export const ActivityProvider: React.FunctionComponent<
     [userActivities]
   );
 
-  const deleteUserActivities = useCallback(() => {
+  const deleteUserActivities = useCallback(async () => {
     // deleteActivities handles errors by itself
-    deleteActivities();
+    await deleteActivities();
   }, [deleteActivities]);
 
   const contextValue: ActivityContextType = useMemo(
