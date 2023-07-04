2c6a854a129083843e5f2fc93e61b11d11f29694
aurelticot <aurelticot.dev@gmail.com>
2023-07-04 17:27:55 +1000

    Define new constants and types for Verida feature

--- src/features/verida/types/index.ts
+++ src/features/verida/types/index.ts
@@ -3,3 +3,24 @@ import { z } from "zod";
 import { VeridaBaseRecordSchema } from "~/features/verida";
 
 export type VeridaBaseRecord = z.infer<typeof VeridaBaseRecordSchema>;
+
+export type VerifiableCredential = {
+  credentialData: {
+    credentialSchema: { id: string };
+    issuer: string;
+  };
+  credentialSchema: string;
+};
+
+export type ReceivedMessage<D> = {
+  data: {
+    data: D[];
+    replyId: string;
+  };
+};
+
+export type SentMessage = {
+  id: string;
+  ok: boolean;
+  rev: string;
+};
