8a2428a67611eb73a9dfdafc7d8646cdc77967fa
aurelticot <aurelticot.dev@gmail.com>
2023-07-10 16:30:56 +1000

    Implement generic activity helper to check requested data via message

--- src/features/activity/utils/index.ts
+++ src/features/activity/utils/index.ts
@@ -1,4 +1,7 @@
-import { type IDatastore } from "@verida/types";
+import type { UseMutateAsyncFunction } from "@tanstack/react-query";
+import type { IDatastore, IMessaging } from "@verida/types";
+import type { WebUser } from "@verida/web-helpers";
+import toast from "react-hot-toast";
 
 import {
   UserActivityRecordSchema,
@@ -8,6 +11,8 @@ import type {
   UserActivity,
   UserActivityRecord,
 } from "~/features/activity/types";
+import { Sentry } from "~/features/sentry";
+import type { ReceivedMessage } from "~/features/verida";
 
 export async function getActivitiesFromDatastore(datastore: IDatastore | null) {
   if (!datastore) {
@@ -79,3 +84,77 @@ export async function deleteActivitiesInDatastore(
     throw new Error("Error deleting activities", { cause: error });
   }
 }
+
+export type HandleInitActivityArgs = {
+  activityId: string;
+  userActivity: UserActivity | null;
+  veridaWebUser: WebUser;
+  verifyReceivedMessage: (message: ReceivedMessage<unknown>) => boolean;
+  saveActivity: UseMutateAsyncFunction<void, unknown, UserActivity>;
+  successToastMessage: string;
+};
+
+export type HandleInitActivity = (
+  args: HandleInitActivityArgs
+) => Promise<() => Promise<void>>;
+
+export const handleInitActivityCheckMessage: HandleInitActivity = async ({
+  activityId,
+  userActivity,
+  veridaWebUser,
+  verifyReceivedMessage,
+  saveActivity,
+  successToastMessage,
+}) => {
+  const checkMessage = async (message: ReceivedMessage<unknown>) => {
+    try {
+      const verified = verifyReceivedMessage(message);
+      if (!verified) {
+        return;
+      }
+
+      await saveActivity({
+        id: activityId,
+        status: "completed",
+        data: {},
+      });
+
+      toast.success(successToastMessage);
+    } catch (error: unknown) {
+      Sentry.captureException(error);
+    }
+  };
+
+  let messaging: IMessaging | undefined;
+  try {
+    const context = await veridaWebUser.getContext();
+    messaging = await context.getMessaging();
+
+    const existingRequestId = userActivity?.data?.requestId;
+
+    if (existingRequestId) {
+      const messages = (await messaging.getMessages()) as
+        | ReceivedMessage<unknown>[]
+        | undefined;
+
+      if (messages) {
+        void Promise.allSettled(
+          messages
+            .filter((message) => message.data.replyId === existingRequestId)
+            .map(checkMessage)
+        );
+      }
+    }
+
+    void messaging.onMessage(checkMessage);
+  } catch (error: unknown) {
+    Sentry.captureException(error);
+  }
+  return async () => {
+    try {
+      if (messaging) await messaging.offMessage(checkMessage);
+    } catch (error: unknown) {
+      Sentry.captureException(error);
+    }
+  };
+};
