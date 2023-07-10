f7bc8d2250e64ec12841610ed2994ee235240e74
aurelticot <aurelticot.dev@gmail.com>
2023-07-10 17:17:05 +1000

    Implement GateKeeper adopter claim activity

--- src/features/activity/activities/claim-gatekeeper-adopter-credential/index.ts
+++ src/features/activity/activities/claim-gatekeeper-adopter-credential/index.ts
@@ -13,10 +13,10 @@ import {
   sendDataRequest,
 } from "~/features/verida";
 
-import { POLYGON_ID_KYC_AGE_VC_SCHEMA_URL } from "./constants";
+import { GATEKEEPER_ADOPTER_VC_SCHEMA_URL } from "./constants";
 import { verifyReceivedMessage } from "./utils";
 
-const ACTIVITY_ID = "claim-polygon-id"; // Never change the id
+const ACTIVITY_ID = "claim-gatekeeper-adopter-credential"; // Never change the id
 
 const handleInit: ActivityOnInit = async (
   veridaWebUser,
@@ -31,20 +31,20 @@ const handleInit: ActivityOnInit = async (
     saveActivity,
     // TODO: Make a localised message of this message
     successToastMessage:
-      "Congrats, you have completed the activity 'Claim a Polygon ID Age credential'",
+      "Congrats, you have completed the activity 'Claim a GateKeeper Adopter credential'",
   });
 };
 
 const handleExecute: ActivityOnExecute = async (veridaWebUser) => {
   try {
     // TODO: Make a localised message of this message
-    const message = "Please share a Polygon ID KYC Age credential";
+    const message = "Please share a GateKeeper Adopter credential";
 
     const sentMessage = await sendDataRequest(veridaWebUser.current, {
       messageSubject: message,
       requestSchema: VAULT_CREDENTIAL_SCHEMA_URL,
       filter: {
-        credentialSchema: POLYGON_ID_KYC_AGE_VC_SCHEMA_URL,
+        credentialSchema: GATEKEEPER_ADOPTER_VC_SCHEMA_URL,
       },
     });
 
@@ -54,9 +54,9 @@ const handleExecute: ActivityOnExecute = async (veridaWebUser) => {
         requestId: sentMessage?.id,
       },
       message: defineMessage({
-        id: "activities.claimPolygonId.executePendingMessage",
+        id: "activities.claimGateKeperAdopter.executePendingMessage",
         defaultMessage:
-          "A request has been sent to your Wallet inbox. Please check your inbox and share a Polygon ID KYC Age credential.",
+          "A request has been sent to your Wallet inbox. Please check your inbox and share a GateKeeper Adopter credential.",
         description:
           "Message explaining a request has been sent to the their Wallet inbox",
       }),
@@ -66,7 +66,7 @@ const handleExecute: ActivityOnExecute = async (veridaWebUser) => {
     return {
       status: "todo",
       message: defineMessage({
-        id: "activity.claimPolygonId.gettingExecutionErrorMessage",
+        id: "activity.claimGateKeperAdopter.gettingExecutionErrorMessage",
         defaultMessage: `There was an error while sending you the credential request, please try again later`,
         description: "Error message when we can't get the user profile",
       }),
@@ -79,40 +79,42 @@ export const activity: Activity = {
   missionId: MISSION_02_ID,
   enabled: false,
   visible: false,
-  order: 1,
+  order: 2,
   points: 50,
   title: defineMessage({
-    id: "activities.claimPolygonId.title",
-    defaultMessage: "Claim a Polygon ID Age credential",
-    description: "Title of the activity 'claim Polygon ID'",
+    id: "activities.claimGateKeperAdopter.title",
+    defaultMessage: "Claim a GateKeeper Adopter credential",
+    description: "Title of the activity 'Claim Gatekeeper Adopter credential'",
   }),
   shortDescription: defineMessage({
-    id: "activities.claimPolygonId.shortDescription",
+    id: "activities.claimGateKeperAdopter.shortDescription",
     defaultMessage:
-      "Go to the Polygon ID issuer demo app and claim a KYC Age Credential, then share this credential by replying to the message you received in your inbox.",
-    description: "Short description of the activity 'claim Polygon ID'",
+      "Go to the GateKeeper claim page and start the process. You will have the connect with your Verida Wallet and then claim the Credential by scanning the QR code again. Once saved in your Wallet, share this credential by replying to the message you received in your inbox.",
+    description:
+      "Short description of the activity 'claim GateKeeper Adopter credential'",
   }),
   actionLabel: defineMessage({
-    id: "activities.claimPolygonId.actionLabel",
+    id: "activities.claimGateKeperAdopter.actionLabel",
     defaultMessage: "Send Request",
-    description: "Label of the button to start the activity claim polygon id",
+    description:
+      "Label of the button to start the activity claim GateKeeper Adopter credential",
   }),
   actionExecutingLabel: defineMessage({
-    id: "activities.claimPolygonId.actionExecutingLabel",
+    id: "activities.claimGateKeperAdopter.actionExecutingLabel",
     defaultMessage: "Sending Request",
     description:
-      "Label of the button when the activity 'claim Polygon ID' is being executed",
+      "Label of the button when the activity 'claim GateKeeper Adopter credential' is being executed",
   }),
   onInit: handleInit,
   onExecute: handleExecute,
   resources: [
     {
       label: defineMessage({
-        id: "activities.claimPolygonId.resources.polygonIdDemoAppUrl.label",
-        defaultMessage: "Polygon ID demo app",
-        description: "Label of the resource 'Polygon ID issuer demo app'",
+        id: "activities.claimGateKeperAdopter.resources.gatekeeperClaimPageUrl.label",
+        defaultMessage: "GateKeeper claim page",
+        description: "Label of the resource 'GateKeeper claim page'",
       }),
-      url: "https://issuer-demo-testing-mainnet.polygonid.me/",
+      url: "https://gatekeeper.software/claim?vcId=582636f6-a43d-46f8-9d7c-d50424e92d93",
     },
   ],
 };
--- src/features/activity/activities/index.ts
+++ src/features/activity/activities/index.ts
@@ -1,3 +1,4 @@
+import { activity as activityClaimGateKeeperAdopter } from "./claim-gatekeeper-adopter-credential";
 import { activity as activityClaimPolygonId } from "./claim-polygon-id";
 import { activity as activityClaimSocialMediaData } from "./claim-social-media-data";
 import { activity as activityCreateVeridaIdentity } from "./create-verida-identity";
@@ -11,5 +12,6 @@ export const activities = [
   activityReferFriend,
   activityUseMarkdownEditor,
   activityClaimPolygonId,
+  activityClaimGateKeeperAdopter,
   activityClaimSocialMediaData,
 ].sort((a, b) => a.order - b.order);
