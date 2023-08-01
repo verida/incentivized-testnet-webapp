d5a0a0c2b42b2206f6cb5b2bc74632b9521f3e71
aurelticot <aurelticot.dev@gmail.com>
2023-08-01 17:42:51 +1000

    Hide instructions and resources for activity 'create-verida-identity'

--- src/features/activity/activities/create-verida-identity/index.ts
+++ src/features/activity/activities/create-verida-identity/index.ts
@@ -58,7 +58,7 @@ export const activity: Activity = {
   longDescription: defineMessage({
     id: "activities.createVeridaIdentity.longDescription",
     defaultMessage:
-      "Verida identities are an implementation of the Decentralized Identifier (DID) Standard from the W3C. A DID has a unique address (e.g. did:vda:testnet:0x6B2...a6F) that is controlled by an end user with a private key or seed phrase. A user can verify themselves to another user, entity or application by sharing their unique DID address.{newline}{newline}Step 1. Install the Verida Wallet (link in resources below) and follow the onboarding to create an identity.{newline}{newline}Step 2. Connect to this Verida Missions webapp with the Verida Wallet, then click the 'Verify' button below.",
+      "Verida identities are an implementation of the Decentralized Identifier (DID) Standard from the W3C. A DID has a unique address (e.g. did:vda:testnet:0x6B2...a6F) that is controlled by an end user with a private key or seed phrase. A user can verify themselves to another user, entity or application by sharing their unique DID address.",
     description: "Long description of the activity 'create identity'",
   }),
   actionLabel: defineMessage({
@@ -73,23 +73,23 @@ export const activity: Activity = {
       "Label of the button when the activity 'create Verida Identity' is being executed",
   }),
   resources: [
-    {
-      label: defineMessage({
-        id: "activities.createVeridaIdentity.resources.howToCreateVeridaIdentity.label",
-        defaultMessage: "User Guide: How to create a Verida Identity",
-        description: "Label of the resource 'How to create a Verida Identity'",
-      }),
-      url: "https://community.verida.io/user-guides/create-a-verida-identity-guide",
-    },
-    {
-      label: defineMessage({
-        id: "activities.createVeridaIdentity.resources.howToCreateVeridaIdentityVideo.label",
-        defaultMessage: "Video: How to create a Verida Identity",
-        description:
-          "Label of the resource 'How to create a Verida Identity' video",
-      }),
-      url: "https://youtu.be/Iav2TRzBiIs",
-    },
+    // {
+    //   label: defineMessage({
+    //     id: "activities.createVeridaIdentity.resources.howToCreateVeridaIdentity.label",
+    //     defaultMessage: "User Guide: How to create a Verida Identity",
+    //     description: "Label of the resource 'How to create a Verida Identity'",
+    //   }),
+    //   url: "https://community.verida.io/user-guides/create-a-verida-identity-guide",
+    // },
+    // {
+    //   label: defineMessage({
+    //     id: "activities.createVeridaIdentity.resources.howToCreateVeridaIdentityVideo.label",
+    //     defaultMessage: "Video: How to create a Verida Identity",
+    //     description:
+    //       "Label of the resource 'How to create a Verida Identity' video",
+    //   }),
+    //   url: "https://youtu.be/Iav2TRzBiIs",
+    // },
     {
       label: defineMessage({
         id: "activities.createVeridaIdentity.resources.walletGuideblogPost.label",
@@ -99,23 +99,23 @@ export const activity: Activity = {
       }),
       url: "https://news.verida.io/verida-wallet-the-ultimate-guide-to-getting-started-998a01cc68b7",
     },
-    {
-      label: defineMessage({
-        id: "activities.createVeridaIdentity.resources.walletInstallLink.label",
-        defaultMessage: "Install the Verida Wallet (iOS and Android)",
-        description: "Label of the wallet install url resource",
-      }),
-      url: "https://vault.verida.io/",
-    },
+    // {
+    //   label: defineMessage({
+    //     id: "activities.createVeridaIdentity.resources.walletInstallLink.label",
+    //     defaultMessage: "Install the Verida Wallet (iOS and Android)",
+    //     description: "Label of the wallet install url resource",
+    //   }),
+    //   url: "https://vault.verida.io/",
+    // },
   ],
-  video: {
-    label: defineMessage({
-      id: "activities.createVeridaIdentity.video.label",
-      defaultMessage: "How to create a Verida Identity",
-      description: "Label of the video 'How to create a Verida Identity'",
-    }),
-    url: "https://www.youtube.com/watch?v=Iav2TRzBiIs",
-  },
+  // video: {
+  //   label: defineMessage({
+  //     id: "activities.createVeridaIdentity.video.label",
+  //     defaultMessage: "How to create a Verida Identity",
+  //     description: "Label of the video 'How to create a Verida Identity'",
+  //   }),
+  //   url: "https://www.youtube.com/watch?v=Iav2TRzBiIs",
+  // },
   onInit: handleInit,
   onExecute: handleExecute,
 };
