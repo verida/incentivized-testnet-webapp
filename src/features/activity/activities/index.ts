b1f9686d8d3c71e81f68b947a8639e17fc20034a
aurelticot <aurelticot.dev@gmail.com>
2023-09-21 17:40:46 +1000

    clean up unused code

--- src/features/activity/activities/index.ts
+++ src/features/activity/activities/index.ts
@@ -1,6 +1,4 @@
 import { activity as activityClaimGateKeeperAdopter } from "./claim-gatekeeper-adopter-credential";
-import { activity as activityClaimPolygonId } from "./claim-polygon-id";
-import { activity as activityClaimSocialMediaData } from "./claim-social-media-data";
 import { activity as activityCreateVeridaIdentity } from "./create-verida-identity";
 import { activity as activityReferFriend } from "./refer-friend";
 import { activity as activityUpdateProfile } from "./update-profile";
@@ -11,7 +9,5 @@ export const activities = [
   activityUpdateProfile,
   activityReferFriend,
   activityUseMarkdownEditor,
-  activityClaimPolygonId,
   activityClaimGateKeeperAdopter,
-  activityClaimSocialMediaData,
 ].sort((a, b) => a.order - b.order);
