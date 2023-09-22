50108a03483e8e8b7303a57d5efaebf8524d15e0
aurelticot <aurelticot.dev@gmail.com>
2023-09-22 19:33:57 +1000

    Implement Gamer31 Twitch activity

--- src/features/activity/activities/gamer31/index.ts
+++ src/features/activity/activities/gamer31/index.ts
@@ -1,3 +1,7 @@
 import { activity as gamner31SteamActivity } from "./claim-gamer31-steam-membership-credential";
+import { activity as gamner31TwitchActivity } from "./claim-gamer31-twitch-membership-credential";
 
-export const gamer31Activities = [gamner31SteamActivity];
+export const gamer31Activities = [
+  gamner31SteamActivity,
+  gamner31TwitchActivity,
+];
