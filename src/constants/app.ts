ad001fa44fb39272dc6bb971e57f79e1770b4a42
aurelticot <aurelticot.dev@gmail.com>
2023-12-11 12:58:28 +1100

    feat: update meta title and description

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -8,7 +8,7 @@ export const APP_TITLE = "Verida Missions";
 export const DEFAULT_META_TITLE = defineMessage({
   id: "app.meta.defaultTitle",
   defaultMessage:
-    "Verida Missions: Explore Decentralized Identity and Private Data Storage",
+    "Verida Missions: Discover the future of personal data control and ownership.",
   description: "Default application title for the meta tags",
 });
 
@@ -16,6 +16,6 @@ export const DEFAULT_META_TITLE = defineMessage({
 export const DEFAULT_META_DESCRIPTION = defineMessage({
   id: "app.meta.defaultDescription",
   defaultMessage:
-    "Discover the future of web3 on the Verida Network. Join the incentivized testnet and experience secure decentralized data storage, zero-knowledge credentials, and more.",
+    "Explore a new paradigm shift for managing your personal data with self-sovereign technology. Experience Verida Wallet, with zero-knowledge credentials, decentralized identity and access to a private data storage network designed for web3.",
   description: "Default application description for the meta tags",
 });
