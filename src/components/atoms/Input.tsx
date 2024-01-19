75bf455950fdf060c2ab1ed8aeff9b024b855dd3
aurelticot <aurelticot.dev@gmail.com>
2024-01-19 17:21:46 +1100

    fix: fix Input component style

--- src/components/atoms/Input.tsx
+++ src/components/atoms/Input.tsx
@@ -7,7 +7,7 @@ export const Input: React.FunctionComponent<InputProps> = (props) => {
   const { className, ...otherProps } = props;
 
   const classes = twMerge(
-    "w-full rounded-xl border border-solid border-transparent bg-primary/10 text-base leading-5 py-[calc(0.625rem_-_1px)] pl-3 pr-10 hover:border-primary/15 focus:border-primary/15 focus:bg-[inherit] focus:outline-none",
+    "w-full rounded-xl border border-solid border-transparent bg-primary/10 text-base sm:text-desktop-base py-[calc(0.625rem_-_1px)] px-[calc(0.625rem_-_1px)] hover:border-primary/15 focus:border-primary/15 focus:bg-[inherit] focus:outline-none",
     className
   );
 
