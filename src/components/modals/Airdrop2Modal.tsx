848be5e7c46f899a5c43fc07c60ca871f913e192
aurelticot <aurelticot.dev@gmail.com>
2024-05-03 17:55:43 +1000

    feat: update messages

--- src/components/modals/Airdrop2Modal.tsx
+++ src/components/modals/Airdrop2Modal.tsx
@@ -1,21 +1,48 @@
 import React from "react";
 import { useIntl } from "react-intl";
 
-import { Typography } from "~/components/atoms";
+import { Icon, Typography } from "~/components/atoms";
 import { Modal } from "~/components/templates";
-import { useAirdrop2 } from "~/features/airdrops";
+import { useAirdrop2, useAirdrop2Queries } from "~/features/airdrops";
 
 export const Airdrop2Modal: React.FunctionComponent = () => {
   const { metadata, isEnabled, isModalOpen, closeModal } = useAirdrop2();
+  const { isEligible, isCheckingEligibility } = useAirdrop2Queries();
 
   const i18n = useIntl();
 
   const modalTitle = i18n.formatMessage(metadata.longTitle);
 
-  const message = i18n.formatMessage({
-    id: "message.message",
-    defaultMessage: "Check your eligibility to the Airdrop",
-    description: "",
+  const checkingEligibilityMessage = i18n.formatMessage({
+    id: "Airdrop2Modal.checkingEligibilityMessage",
+    defaultMessage: "Checking your eligibility to the airdrop...",
+    description:
+      "Message displayed in the airdrop 2 modal when checking if eligible",
+  });
+
+  const succesfullyEligibleMessage = i18n.formatMessage(
+    {
+      id: "Airdrop2Modal.succesfullyEligibleMessage",
+      defaultMessage:
+        "Congratulations! You are eligible for the Galxe and Zealy participants Airdrop.{newline}{newline}Check our socials to be notified when the claim window opens.",
+      description:
+        "Message displayed in the airdrop 2 modal when the user is eligible",
+    },
+    {
+      newline: (
+        <>
+          <br />
+        </>
+      ),
+    }
+  );
+
+  const notEligibleMessage = i18n.formatMessage({
+    id: "Airdrop2Modal.notEligibleMessage",
+    defaultMessage:
+      "Unfortunately, you are not eligible for the Galxe and Zealy participants Airdrop.",
+    description:
+      "Message displayed in the airdrop 2 modal when the user is not eligible",
   });
 
   return (
@@ -26,7 +53,20 @@ export const Airdrop2Modal: React.FunctionComponent = () => {
     >
       <div className="flex flex-col gap-6">
         <div className="flex flex-col sm:flex-row gap-4 items-center">
-          <Typography variant="base">{message}</Typography>
+          {isCheckingEligibility ? (
+            <Icon type="loading" size={40} className="animate-spin-slow" />
+          ) : isEligible ? (
+            <Icon type="check" size={40} className="text-success" />
+          ) : (
+            <Icon type="notification-error" size={40} className="text-error" />
+          )}
+          <Typography variant="base">
+            {isCheckingEligibility
+              ? checkingEligibilityMessage
+              : isEligible
+                ? succesfullyEligibleMessage
+                : notEligibleMessage}
+          </Typography>
         </div>
       </div>
     </Modal>
