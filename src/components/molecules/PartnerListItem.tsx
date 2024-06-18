ebea83f0132ace678afb03c4c23056c53424184e
aurelticot <aurelticot.dev@gmail.com>
2024-06-18 13:54:15 +1000

    refactor: move link to partner list

--- src/components/molecules/PartnerListItem.tsx
+++ src/components/molecules/PartnerListItem.tsx
@@ -1,5 +1,4 @@
 import { useIntl } from "react-intl";
-import { Link } from "react-router-dom";
 
 import { Typography } from "~/components/atoms";
 import { Partner } from "~/features/partners";
@@ -27,18 +26,16 @@ export const PartnerListItem: React.FC<PartnersListItemProps> = (props) => {
   // TODO: Use container query rather than media query to adapt the content based on the container size for smoother transitions
   return (
     <article {...articleProps}>
-      <Link to={`/partners/${partner.id}`}>
-        <div className="border border-border hover:border-border-hover bg-background-light hover:bg-background-extra-light rounded-3xl md:rounded-10 flex flex-col items-center justify-center py-8 lg:py-12 gap-6 md:gap-10">
-          <img
-            src={partner.logo}
-            alt={partnerLogoAlt}
-            className="bg-white rounded-2xl md:rounded-3xl p-4 md:p-6 w-20 md:w-30"
-          />
-          <Typography variant="heading-m" component="p" className="text-center">
-            {partner.name}
-          </Typography>
-        </div>
-      </Link>
+      <div className="border border-border hover:border-border-hover bg-background-light hover:bg-background-extra-light rounded-3xl md:rounded-10 flex flex-col items-center justify-center py-8 lg:py-12 gap-6 md:gap-10">
+        <img
+          src={partner.logo}
+          alt={partnerLogoAlt}
+          className="bg-white rounded-2xl md:rounded-3xl p-4 md:p-6 w-20 md:w-30"
+        />
+        <Typography variant="heading-m" component="p" className="text-center">
+          {partner.name}
+        </Typography>
+      </div>
     </article>
   );
 };
