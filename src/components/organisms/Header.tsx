d88f24b775e085e415c84cc721dd7a8ce1dab2ab
aurelticot <aurelticot.dev@gmail.com>
2024-06-19 19:11:50 +1000

    feat: add nav in header

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,8 +1,10 @@
 import React, { useCallback, useState } from "react";
+import { useIntl } from "react-intl";
+import { NavLink } from "react-router-dom";
 import { twMerge } from "tailwind-merge";
 
 import { ReactComponent as VeridaNetworkLogoWithText } from "~/assets/images/verida_network_logo_with_text.svg";
-import { Avatar, Icon } from "~/components/atoms";
+import { Avatar, Icon, Typography } from "~/components/atoms";
 import {
   HeaderIdentityMenu,
   HeaderNavMenu,
@@ -41,24 +43,71 @@ export const Header: React.FunctionComponent<HeaderProps> = (props) => {
     setOpenNavMenu(false);
   }, []);
 
+  const i18n = useIntl();
+
+  const missionsNavLabel = i18n.formatMessage({
+    id: "Header.missionsNavLabel",
+    description: "Label for the Mission item in the navigation menu",
+    defaultMessage: "Missions",
+  });
+
+  const airdropsNavLabel = i18n.formatMessage({
+    id: "Header.airdropsNavLabel",
+    description: "Label for the Airdrops item in the navigation menu",
+    defaultMessage: "Airdrops",
+  });
+
   return (
     <header {...headerProps}>
       <div
         className={twMerge(
-          "flex flex-row justify-between border-b border-solid border-divider px-4 pl-0 pt-3 pb-[calc(0.75rem_-_1px)] backdrop-blur-xl sm:px-6",
+          "flex flex-row justify-between border-b border-solid border-divider backdrop-blur-xl",
           openNavMenu ? "bg-background" : "bg-background/40"
         )}
       >
-        <div className="justify-self-start flex flex-row items-center justify-stretch">
-          <button onClick={handleOpenNavMenu} className="pl-4 pr-3 py-1">
-            <Icon type={openNavMenu ? "menu-close" : "menu-open"} size={24} />
-          </button>
-          <HeaderNavMenu open={openNavMenu} onClose={handleCloseNavMenu} />
-          <div className="h-8 aspect-[10/3]">
-            <VeridaNetworkLogoWithText height="100%" width="100%" />
+        <div className="flex flex-row gap-10">
+          <div className="justify-self-start pl-0 pt-3 pb-[calc(0.75rem_-_1px)] flex flex-row items-center">
+            <button
+              onClick={handleOpenNavMenu}
+              className="sm:hidden pl-4 pr-3 py-1"
+            >
+              <Icon type={openNavMenu ? "menu-close" : "menu-open"} size={24} />
+            </button>
+            <HeaderNavMenu open={openNavMenu} onClose={handleCloseNavMenu} />
+            <div className="sm:pl-6 h-8 sm:h-10 aspect-[10/3]">
+              <VeridaNetworkLogoWithText height="100%" width="100%" />
+            </div>
+          </div>
+          <div className="hidden sm:flex flex-row gap-4">
+            <NavLink
+              to={"/"}
+              className={({ isActive }) =>
+                twMerge(
+                  "px-4 pt-px flex items-center border-b",
+                  isActive
+                    ? "text-foreground border-transparent-95"
+                    : "text-transparent-70 hover:text-foreground border-transparent"
+                )
+              }
+            >
+              <Typography>{missionsNavLabel}</Typography>
+            </NavLink>
+            <NavLink
+              to={"/airdrops"}
+              className={({ isActive }) =>
+                twMerge(
+                  "px-4 pt-px flex items-center border-b",
+                  isActive
+                    ? "text-foreground border-transparent-95"
+                    : "text-transparent-70 hover:text-foreground border-transparent"
+                )
+              }
+            >
+              <Typography>{airdropsNavLabel}</Typography>
+            </NavLink>
           </div>
         </div>
-        <div className="flex items-center gap-2 md:gap-4 justify-between justify-self-end">
+        <div className="justify-self-end pr-4 sm:pr-6 pt-3 pb-[calc(0.75rem_-_1px)] flex items-center gap-2 md:gap-4 justify-between">
           {isConnected ? (
             <>
               <XpPointsChip
