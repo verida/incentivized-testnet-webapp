010cb19d1698989c843ecbeceef2a18890ac6cc4
aurelticot <aurelticot.dev@gmail.com>
2024-06-20 16:45:40 +1000

    feat: add FAQ and about to airdrops page

--- src/components/pages/AirdropsPage.tsx
+++ src/components/pages/AirdropsPage.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import { useIntl } from "react-intl";
 import { Outlet } from "react-router-dom";
 
-import { Typography } from "~/components/atoms";
+import { ButtonLink, Typography } from "~/components/atoms";
 import { AirdropCard } from "~/components/organisms/AirdropCard";
 import { PageLayout } from "~/components/templates";
-import { airdrops } from "~/features/airdrops";
+import {
+  AIRDROPS_COMMON_QUESTIONS,
+  AIRDROPS_FAQ_URL,
+  airdrops,
+} from "~/features/airdrops";
 
 export const AirdropsPage: React.FC = () => {
   const i18n = useIntl();
@@ -32,6 +36,40 @@ export const AirdropsPage: React.FC = () => {
       " This program is intended to reward early adopters to the network and encourage newcomers to discover Verida's features and capabilities.",
   });
 
+  const aboutAirdropsSectionTitle = i18n.formatMessage({
+    id: "AirdropsPage.aboutAirdropsSectionTitle",
+    description: "Title of the section about airdrops in the airdrops page",
+    defaultMessage: "About Airdrops",
+  });
+
+  const aboutAirdropsMessage = i18n.formatMessage(
+    {
+      id: "AirdropsPage.aboutAirdropsMessage",
+      description: "Message in the section about airdrops in the airdrops page",
+      defaultMessage:
+        "The Verida Foundation has committed to giving 20% of the total initial token supply to the community through its Network Growth Rewards.{newline}{newline}Airdrops target constructive participation, ensuring that tokens go to contributors. Past airdrop eligibility does not guarantee eligibility in future airdrops.",
+    },
+    {
+      newline: (
+        <>
+          <br />
+        </>
+      ),
+    }
+  );
+
+  const commonQuestionsSectionTitle = i18n.formatMessage({
+    id: "AirdropsPage.commonQuestionsSectionTitle",
+    description: "Title of the section common questions in the airdrops page",
+    defaultMessage: "Common Questions",
+  });
+
+  const faqButtonLabel = i18n.formatMessage({
+    id: "AirdropsPage.faqButtonLabel",
+    description: "Label for the FAQ button in the airdrops page",
+    defaultMessage: "Read FAQ",
+  });
+
   return (
     <>
       <PageLayout
@@ -51,15 +89,66 @@ export const AirdropsPage: React.FC = () => {
               <Typography className="text-center">{tagline}</Typography>
             </div>
           </div>
-          <ul className="flex flex-col gap-10">
-            {airdrops.map((airdrop) => (
-              <li key={airdrop.id}>
-                <article>
-                  <AirdropCard airdrop={airdrop} />
-                </article>
-              </li>
-            ))}
-          </ul>
+          <div className="flex flex-col gap-28">
+            <ul className="flex flex-col gap-10">
+              {airdrops.map((airdrop) => (
+                <li key={airdrop.id}>
+                  <article>
+                    <AirdropCard airdrop={airdrop} />
+                  </article>
+                </li>
+              ))}
+            </ul>
+            <div className="flex flex-col gap-4 items-center">
+              <Typography
+                variant="heading-l"
+                component="p"
+                className="text-center"
+              >
+                {aboutAirdropsSectionTitle}
+              </Typography>
+              <Typography
+                variant="base"
+                className="text-muted-foreground text-center"
+              >
+                {aboutAirdropsMessage}
+              </Typography>
+            </div>
+            <div className="flex flex-col gap-4 items-center">
+              <Typography
+                variant="heading-l"
+                component="p"
+                className="text-center"
+              >
+                {commonQuestionsSectionTitle}
+              </Typography>
+              <div className="flex flex-col gap-6">
+                {AIRDROPS_COMMON_QUESTIONS.map((question, index) => (
+                  <div key={index} className="flex flex-col gap-3">
+                    <Typography variant="heading-s" component="p">
+                      {i18n.formatMessage(question.question)}
+                    </Typography>
+                    <Typography className="text-muted-foreground">
+                      {i18n.formatMessage(question.answer, {
+                        newline: (
+                          <>
+                            <br />
+                          </>
+                        ),
+                      })}
+                    </Typography>
+                  </div>
+                ))}
+              </div>
+              <ButtonLink
+                href={AIRDROPS_FAQ_URL}
+                variant="contained"
+                color="primary"
+              >
+                {faqButtonLabel}
+              </ButtonLink>
+            </div>
+          </div>
         </div>
       </PageLayout>
       <Outlet />
--- src/features/airdrops/constants/index.ts
+++ src/features/airdrops/constants/index.ts
@@ -1,4 +1,5 @@
 export * from "./airdrops";
 export * from "./airdrop1";
 export * from "./airdrop2";
+export * from "./faq";
 export * from "./urls";
