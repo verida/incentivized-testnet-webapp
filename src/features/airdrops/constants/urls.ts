24197ad7594d212892cd6ef6b4e2cd244934dc2a
aurelticot <aurelticot.dev@gmail.com>
2024-06-24 16:22:02 +1000

    feat: update URL to airdrop links

--- src/features/airdrops/constants/urls.ts
+++ src/features/airdrops/constants/urls.ts
@@ -1,5 +1,5 @@
 export const AIRDROPS_TERMS_URL =
-  "https://community.verida.io/verida-token-vda/airdrops-faq/airdrop-t-and-cs";
+  "https://community.verida.io/verida-token-vda/rewards-and-incentives/airdrop-t-and-cs";
 
 export const AIRDROPS_FAQ_URL =
-  "https://community.verida.io/verida-token-vda/airdrops-faq";
+  "https://community.verida.io/verida-token-vda/rewards-and-incentives/airdrop-faqs";
